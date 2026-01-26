4be2c1451573097cb2d64f6431156224bc1eaa7a
Nic Strike <npstrike.github@randomranger.com>
2026-01-25 21:55:17 -0600

    Added gpu stats support

--- stats_host.py
+++ stats_host.py
@@ -69,27 +69,39 @@ if __name__ == '__main__':
             mem_percentage_used = round(memory_stats.percent)
 
             d3_query_result = subprocess.run(['cat', '/sys/class/drm/card1/device/power_state'], stdout=subprocess.PIPE).stdout
+            d3Cold = 1 if "D3cold" in str(d3_query_result) else 0
+
+            gpu_pwr = 0
+            gpu_pwr_cap = 120
+            
+            time_since_last_T_read = datetime.now() - last_temp_reading
+            if time_since_last_T_read.seconds > max_d0_temp_readrate and d3Cold == 0:
+                gpu_pwr_result = str(subprocess.run(['cat', '/sys/class/drm/card1/device/hwmon/hwmon9/power1_average'], stdout=subprocess.PIPE).stdout)
+                gpu_pwr = int(int(re.search("(\\d+)", str(gpu_pwr_result))[1]) / 1000000)
+                gpu_pwr_cap_result = str(subprocess.run(['cat', '/sys/class/drm/card1/device/hwmon/hwmon9/power1_cap'], stdout=subprocess.PIPE).stdout)
+                gpu_pwr_cap = int(int(re.search("(\\d+)", str(gpu_pwr_cap_result))[1]) / 1000000)
+            # print(f"GPU Power: {gpu_pwr}/{gpu_pwr_cap}")
 
             vol_query_result = subprocess.run(['amixer', 'sget', 'Master'], stdout=subprocess.PIPE).stdout
             vol_query_result = str(vol_query_result)
-            # print(vol_query_result)
             current_vol = int(re.search("(\\d+)%", vol_query_result)[1])
             muted = 1 if "[off]" in vol_query_result else 0
-            print(f"Muted: {muted}")
+            # print(f"Muted: {muted}")
 
             wifi_query_result = str(subprocess.run(['ifconfig', 'wlp5s0'], stdout=subprocess.PIPE).stdout)
             wifi_connected = 1 if "inet" in wifi_query_result else 0
-            print(f"Wifi connected: {wifi_connected}")
+            # print(f"Wifi connected: {wifi_connected}")
 
-            d3Cold = 1 if "D3cold" in str(d3_query_result) else 0
-            time_since_last_T_read = datetime.now() - last_temp_reading
+            gpu_temp = 0
             if time_since_last_T_read.seconds > max_d0_temp_readrate and d3Cold == 0:
                 last_temp_reading = datetime.now()
                 temp_stats = psutil.sensors_temperatures()
+                # gpu_temp = round(temp_stats["amdgpu"][1].current)
             elif d3Cold == 1:
                 temp_stats = psutil.sensors_temperatures()
+            
             cpu_temp = round(temp_stats["cros_ec"][3].current)
-            print(f"CPU: {cpu_percent}% {cpu_temp}c\tRAM: {mem_percentage_used}\tGPU D3Cold: {d3Cold} ({d3_query_result})")
+            print(f"CPU: {cpu_percent}% {cpu_temp}c\tRAM: {mem_percentage_used}\tGPU D3Cold: {d3Cold} ({d3_query_result}) {gpu_temp}c")
 
 
             # pprint(temp_stats)
@@ -102,7 +114,7 @@ if __name__ == '__main__':
             discharging = 0xFF
             if battery.state == "Discharging": discharging = 1
             elif battery.state == "Charging": discharging = 0
-            sys_data = [0x3F, cpu_percent, mem_percentage_used, battery_watts, discharging, cpu_temp, d3Cold, battery_percent, current_vol, muted, wifi_connected] # First byte is "channel id"
+            sys_data = [0x3F, cpu_percent, mem_percentage_used, battery_watts, discharging, cpu_temp, d3Cold, battery_percent, current_vol, muted, wifi_connected, gpu_pwr, gpu_pwr_cap, gpu_temp] # First byte is "channel id"
             # print(sys_data)
         
             send_raw_report(sys_data)
